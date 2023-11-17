31a2fe9962b9fa1293649f4761805f0f5ad6db3d
Amanda Young <botallytonkers@gmail.com>
2023-11-17 13:03:54 -0700

    wipp

--- data/migrations/20231117195025_initial-migration.js
+++ data/migrations/20231117195025_initial-migration.js
@@ -1,27 +1,27 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
+
 exports.up = async function(knex) {
   await knex.schema
   .createTable('recipes', table => {
-    table.increments()
+    table.increments('recipe_id')
+    table.string('recipe_name', 200).notNullable().unique()
   })
+
   .createTable('ingredients', table => {
-    table.increments()
+    table.increments('ingredient_id')
+    table.string('ingredient_name', 200).notNullable().unique()
+    table.string('ingredient_unit', 50)
   })
+
   .createTable('steps', table => {
     table.increments()
   })
+  
   .createTable('step_ingredients', table => {
     table.increments()
   })
 };
 
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
+
 exports.down = async function(knex) {
   await knex.schema
    .dropTableIfExists('step_ingredients')
